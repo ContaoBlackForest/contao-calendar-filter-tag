67f19df130f5a40d4b36ade27878970a098f858a
Sven Baumann <baumann.sv@gmail.com>
2016-02-04 14:30:23 +0100

    create it

---
+++ contao/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * contao-calendar-filter-tag
+ *
+ * Copyright (C) ContaoBlackForest
+ *
+ * @package   contao-calendar-filter-tag
+ * @file      config.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   LGPL-3.0+
+ * @copyright Copyright 2015 ContaoBlackForest
+ */
