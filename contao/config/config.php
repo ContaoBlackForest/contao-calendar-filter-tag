a8b3b4b999c431765179c14e2015d444faa219fb
Sven Baumann <baumann.sv@gmail.com>
2016-02-04 18:57:53 +0100

    register events for dispatcher

--- contao/config/config.php
+++ contao/config/config.php
@@ -3,12 +3,30 @@
 /**
  * contao-calendar-filter-tag
  *
- * Copyright (C) ContaoBlackForest
+ * Copyright © ContaoBlackForest
  *
  * @package   contao-calendar-filter-tag
  * @file      config.php
  * @author    Sven Baumann <baumann.sv@gmail.com>
  * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
  * @license   LGPL-3.0+
- * @copyright Copyright 2015 ContaoBlackForest
+ * @copyright Copyright 2016 ContaoBlackForest
  */
+
+$GLOBALS['TL_EVENTS'][\ContaoBlackForest\Module\CalendarFilter\Event\GetFilterOptionsEvent::NAME][] =
+    array(
+        new \ContaoBlackForest\Module\CalendarFilter\Tag\DataContainer\GetFilterOptions(),
+        'addFilterOptions'
+    );
+
+$GLOBALS['TL_EVENTS'][\ContaoBlackForest\Module\CalendarFilter\Event\PostFilterInformationEvent::NAME][] =
+    array(
+        new \ContaoBlackForest\Module\CalendarFilter\Tag\DataContainer\PostFilterInformation(),
+        'getInformation'
+    );
+
+$GLOBALS['TL_EVENTS'][\ContaoBlackForest\Module\CalendarFilter\Event\PostFilterEventsEvent::NAME][] =
+    array(
+        new \ContaoBlackForest\Module\CalendarFilter\Tag\DataContainer\PostFilterEvents(),
+        'getFilteredEvents'
+    );
